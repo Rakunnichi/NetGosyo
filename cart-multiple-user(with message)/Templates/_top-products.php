a0630a457c94294b0f4974d670182115e70f0bb8
Earl <centinoearlcartney@gmail.com>
2023-07-31 16:24:50 +0800

    fix bug of top products

--- cart-multiple-user(with message)/Templates/_top-products.php
+++ cart-multiple-user(with message)/Templates/_top-products.php
@@ -14,7 +14,7 @@
 
           <div class="item py-2">
             <div class="product font-rale">
-              <a href="<?php printf('%s?id=%s', 'product.php',  $fetch_product['id']); ?>"><img src="image/<?php echo $fetch_product['image']; ?>" alt="product1" class="img-fluid"></a>
+              <a href="<?php printf('%s?id=%s', 'product.php',  $fetch_product['id']); ?>"><img src="Seller-uploads/<?php echo $fetch_product['image']; ?>" alt="product1" class="img-fluid"></a>
               <div class="text-center">
                 <h6><b><?php echo $fetch_product['name'] ?? '0'; ?></b></h6>
                 <div class="rating text-warning font-size-12">
