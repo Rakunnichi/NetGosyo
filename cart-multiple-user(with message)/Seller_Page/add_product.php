830e26a0eefca2370b39850e288faa4e1e3a4914
Earl <centinoearlcartney@gmail.com>
2023-07-31 16:21:27 +0800

    enabled add product

--- cart-multiple-user(with message)/Seller_Page/add_product.php
+++ cart-multiple-user(with message)/Seller_Page/add_product.php
@@ -1,6 +1,38 @@
 <?php
   include('header.php');
-  ?>
+
+
+  if(isset($_POST['addprod-submit'])){
+
+    $prodname = mysqli_real_escape_string($conn, $_POST['prodname']);
+    $prodprice = mysqli_real_escape_string($conn, $_POST['prodprice']);
+    $prodquantity = mysqli_real_escape_string($conn, $_POST['prodquantity']);
+    $prodcategory = mysqli_real_escape_string($conn, $_POST['category']);
+   
+    $file_name = $_FILES['file']['name'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $file_type = $_FILES['file']['type'];
+    $tmp = explode('.', $_FILES['file']['name']);
+    $file_ext = strtolower(end($tmp));
+    $extensions = array("jpeg","jpg","png");
+    
+    if(in_array($file_ext,$extensions) === false){
+        echo "Extension not allowed, please choose a JPEG or PNG file.";
+        exit();
+    }
+    
+    $new_file_name = time().'-'.$file_name;
+   
+    $destination = "../Seller-uploads/".$new_file_name;
+    move_uploaded_file($file_tmp, $destination);
+    
+    mysqli_query($conn, "INSERT INTO `products` (user_id, name, price, image, item_brand, quantity) VALUES('$user_id', '$prodname', '$prodprice', '$new_file_name', '$prodcategory', '$prodquantity')") or die ('query failed');
+       
+    echo "Product uploaded successfully.";
+    
+}
+
+?>
 
 
 <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
@@ -47,10 +79,12 @@
                                     </div>
                                     <div class="col-md-6">
                                         <div class="input-group input-group-outline my-3">
-                                            <input type="text" name="prodprice" placeholder="Product Price"
+                                            <input type="number" name="prodprice" placeholder="Product Price"
                                                 class="form-control">
                                         </div>
                                     </div>
+
+                                    
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
@@ -66,12 +100,20 @@
                                                 <option value="Banig">Banig</option>
                                                 <option value="Clothes">Clothes</option>
                                                 <option value="Food">Food</option>
+                                                <option value="Gadget">Gadgets</option>
                                             </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="input-group input-group-outline my-2">
+                                        <label>Add Image &#8595;</label>
+                                        <input class="form-control" type="file" name="file" style="width:100%;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <button class="btn btn-icon btn-3 button-update" type="button">
+                                <button class="btn btn-icon btn-3 button-update" type="submit" name="addprod-submit">
                                     <span class="btn-inner--icon"><i class="material-icons">add_circle</i></span>
                                     <span class="btn-inner--text">Add Product</span>
                                 </button>
